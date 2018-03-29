a6a5b81e3700fa8e605f009cc67e5fd01613910a
totti05 <raul_leandro_0504@hotmail.com>
2018-03-28 21:18:29 -0400

    se modifico el app.js se configuro para requerir el archivo userRoutes.js y agregaron algunos comentarios

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-app.js
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+//settings
+app.set('port', process.env.PORT || 3000)
+
+//middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+
+//routes
+
+require('./routes/userRoutes.js')(app); 
+
+//static files
+
+app.listen(app.get('port'), ()=> {
+	console.log('server on port: 3000');
+})
+
+
